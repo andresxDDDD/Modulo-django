9cf672ba8e81600adfd40bfd903c3afc18b04469
andresxDDDD <moraga_2006+1@hotmail.com>
2026-05-08 02:06:39 +0000

    Django

--- productos/urls.py
+++ productos/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='home_productos'),
+    path('logins',views.login, name='logins'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Producto
 
 # Create your views here.
-def index(request):
-    return HttpResponse("mi primera app")
+def index(request):#ve a buscar los productos en la base de datos y pasaselos a index
+  productos = Producto.objects.all()
+  return render(request,'productos/home.html',{'productos': productos})
+
+    
+
+def dashboard(request):
+ return HttpResponse("dashboard")
+
+
+def login(request):
+ return HttpResponse("login")
+
+def register(request):
+ return HttpResponse("register")
+
+def register(request):
+ return HttpResponse("vender")
